0863f6f075dcb2b04e7514689f8bca3b70a4f273
HR0101 <hararyuto0101@gmail.com>
2026-08-11 02:43:11 +0900

    Add macOS CI and Xcode project (#1)
    * Add macOS CI and Xcode project
    
    * Include shared models in macOS build

---
+++ TeleDeckMacTests/TeleDeckMacTests.swift
@@ -0,0 +1,19 @@
+//
+//  TeleDeckMacTests.swift
+//  TeleDeckMacTests
+//
+//  Created by hara ryuto   on 2026/07/12.
+//
+
+import Testing
+@testable import TeleDeckMac
+
+struct TeleDeckMacTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
